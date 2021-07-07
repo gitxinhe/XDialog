3405fd388364d1abb52b253b3d154e2171d4690e
10158 <10158@DESKTOP-0SBJVQR>
2021-07-07 17:37:20 +0800

    添加底部弹窗控件

--- XDialog/src/main/java/com/kiloway/xdialog/xenabledialog/XDialogUtils.java
+++ XDialog/src/main/java/com/kiloway/xdialog/xenabledialog/XDialogUtils.java
@@ -15,10 +15,6 @@ public class XDialogUtils {
         XEnsureDialog ensureDialog = new XEnsureDialog.Builder(context)
                 .setMessage(content)
                 .setTitle(title)
-                .setMessageColor(context.getResources().getColor(R.color.black))
-                .setNegativeTextColor(context.getResources().getColor(R.color.black))
-                .setPositiveTextColor(context.getResources().getColor(R.color.black))
-                .setTitleColor(context.getResources().getColor(R.color.black))
                 .setNegativeShow(showCancel)
                 .setNegativeClick(new DialogInterface.OnClickListener() {
                     @Override
